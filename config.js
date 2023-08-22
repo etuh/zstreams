a2b1020d666bbc80cc2d32eb8a31f2b7baa5d7a4
edwin <edwinkuria0@gmail.com>
2023-08-22 12:09:24 +0300

    Fork from movie-web.app

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://zstreams.edwinkuria0-fakedata32535.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OTM2MGZmMWQyNDI5YWEzMDI3OGYxZDk4NTFlYTg0NyIsInN1YiI6IjY0ZTQ3NDA1ZTg5NGE2MDBjNzI2YTU5MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HBfvT93wcZi1lMDXqogNMlPdLX_nYTH3yI4uH3mzRWw"
+};
